22686d89311fa4ab37115cfb75e04a14a05e2250
Nader Ahmad <naderahmad7x@gmail.com>
2021-06-10 13:08:56 -0400

    upload main files
    First version of the web, mobile first

---
+++ main.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('.navbar__btn');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+particlesJS.load('particles-js', 'particles.json');
